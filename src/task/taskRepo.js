136b1152db4924b7c192cdfca39ac246d8277be8
Kizzuato <kyyajuara@gmail.com>
2024-02-02 15:40:10 +0700

    configuring acc and history task

--- src/task/taskRepo.js
+++ src/task/taskRepo.js
@@ -1,26 +1,68 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+// Repo untuk mengedit task
+const updateTaskRepo = async (data) => {
+  return await prisma.task.update({
+    where: {
+        id
+    },
+    data,
+  });
+};
+
+// Repo untuk membuat task baru
 const createTaskRepo = async (data) => {
-    return await prisma.task.create({
-        data
-    })
-}
+  return await prisma.task.create({
+    data,
+  });
+};
 
+//  Repo untuk memperlihatkan task yang sudah di acc dari database
 const getAllTaskRepo = async () => {
-    return await prisma.task.findMany()
-}
+  return await prisma.task.findMany({
+    where: {
+      NOT: {
+        status: "wait-app",
+      },
+    },
+  });
+};
 
+//  Repo untuk memperlihatkan task yang belum di acc
+const getAllWaitedTaskRepo = async () => {
+  return await prisma.task.findMany({
+    where: {
+      status: "wait-app",
+    },
+  });
+};
+
+//  Repo untuk memperlihatkan task yang telah dihapus
+const getAllDeletedTaskRepo = async () => {
+  return await prisma.task.findMany({
+    where: {
+      deleted_at: {
+        NOT: null,
+      },
+    },
+  });
+};
+
+// Repo untuk mencari task berdasarkan Id
 const getTaskByIdRepo = async (id) => {
-    return await prisma.task.findUnique({
-        where: {
-            id
-        }
-    })
-}
+  return await prisma.task.findUnique({
+    where: {
+      id,
+    },
+  });
+};
 
 module.exports = {
-    createTaskRepo,
-    getAllTaskRepo,
-    getTaskByIdRepo
-}
+  updateTaskRepo,
+  createTaskRepo,
+  getAllTaskRepo,
+  getAllWaitedTaskRepo,
+  getAllDeletedTaskRepo,
+  getTaskByIdRepo,
+};
--- src/task/taskServ.js
+++ src/task/taskServ.js
@@ -1,29 +1,71 @@
-const { createTaskRepo, getAllTaskRepo, getTaskByIdRepo } = require("./taskRepo")
+const {
+  updateTaskRepo,
+  createTaskRepo,
+  getAllTaskRepo,
+  getAllWaitedTaskRepo,
+  getAllDeletedTaskRepo,
+  getTaskByIdRepo,
+} = require("./taskRepo");
 
+// Service untuk mengedit task
+const updateTaskServ = async (data) => {
+  const dataRest = {
+    task_type: data.task_type,
+    task_title: data.task_title,
+    priority: data.priority,
+    status: data.status,
+    start_date: data.start_date,
+    due_date: data.due_date,
+    description: data.description,
+    pic: data.pic,
+    spv: data.spv,
+  };
+
+  return await updateTaskRepo(dataRest);
+};
+
+// Service untuk membuat task baru
 const createTaskServ = async (data) => {
-    const dataRest = {
-        task_type : data.task_type,
-        task_title: data.task_title,
-        priority: data.priority,
-        start_date : data.start_date,
-        due_date: data.due_date,
-        description: data.description,
-        pic: data.pic,
-        spv: data.spv,
-    }
+  const dataRest = {
+    task_type: data.task_type,
+    task_title: data.task_title,
+    priority: data.priority,
+    status: data.status,
+    start_date: data.start_date,
+    due_date: data.due_date,
+    description: data.description,
+    pic: data.pic,
+    spv: data.spv,
+  };
 
-    return await createTaskRepo(dataRest)
-}
+  return await createTaskRepo(dataRest);
+};
 
-const getAllTaskserv = async () => {
-    return await getAllTaskRepo()
-}
+//  Service untuk mengambil semua task yang sudah di acc di database
+const getAllTaskServ = async () => {
+  return await getAllTaskRepo();
+};
 
+//  Service untuk mengambil semua task yang belum di acc
+const getAllWaitedTaskServ = async () => {
+  return await getAllWaitedTaskRepo();
+};
+
+// Service untuk mengambil semua histori task yang telah di hapus
+const getAllDeletedTaskServ = async () => {
+  return await getAllDeletedTaskRepo();
+};
+
+// Service untuk mengambil task berdasarkan id
 const getTaskByIdServ = async (id) => {
-    return await getTaskByIdRepo(id)
-}
+  return await getTaskByIdRepo(id);
+};
+
 module.exports = {
-    createTaskServ,
-    getAllTaskserv,
-    getTaskByIdServ
-}
+  updateTaskServ,
+  createTaskServ,
+  getAllTaskServ,
+  getAllWaitedTaskServ,
+  getAllDeletedTaskServ,
+  getTaskByIdServ,
+};
